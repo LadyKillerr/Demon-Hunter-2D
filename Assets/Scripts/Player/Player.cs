7cecb116eebc65c19d69b1d91dbbb78d8394f53a
Lady Killerr <dungvuduc0509@gmail.com>
2024-05-30 22:14:23 +0700

    added walk, idling and roll animations

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,55 +1,130 @@
-using System;
+﻿using System;
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     Rigidbody2D playerRigidbody;
 
-    public Vector3 moveInput;
-    float playerScaleX;
+    Vector2 moveInput;
+    bool IsMovingHorizontal;
+    bool IsMovingVertical;
 
+    Animator playerAnimator;
 
-    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float moveSpeed = 600f;
+    [SerializeField] float rollAnimTime = 0.4f;
+    [SerializeField] float rollSpeedBoost = 400f;
+    [SerializeField] Vector2 rollDistance = new Vector2(20, 0);
 
     private void Awake()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     void Start()
     {
-        playerScaleX = transform.localScale.x;
+
     }
 
-    void Update()
+    // Fixed Update r for playerMovement
+    void FixedUpdate()
     {
+
         CharacterMovement();
         FlipSprite();
+
+    }
+
+    #region playerMovement
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+
+
     }
 
     public void CharacterMovement()
     {
-        moveInput.x = Input.GetAxis("Horizontal");
-        moveInput.y = Input.GetAxis("Vertical");
+        Vector2 playerInput = new(moveInput.x * moveSpeed * Time.fixedDeltaTime,
+                                          moveInput.y * moveSpeed * Time.fixedDeltaTime);
 
-        transform.position += moveSpeed * moveInput * Time.deltaTime;
+        playerRigidbody.velocity = playerInput;
+
+
+        // Walking Animations
+        //if (IsMovingHorizontal || IsMovingVertical)
+        //{
+        //    playerAnimator.SetTrigger("isWalking");
+
+        //}
+        //else if (!IsMovingHorizontal && !IsMovingVertical)
+        //{
+        //    playerAnimator.SetTrigger("isIdling");
+        //}
+
+        // một cách hay hơn để nhận biết khi nào nên bật Walking anim
+        playerAnimator.SetFloat("Speed", moveInput.sqrMagnitude);
+
     }
 
-    public void FlipSprite()
+    void OnRoll(InputValue value)
     {
+        IsMovingHorizontal = Mathf.Abs(playerRigidbody.velocity.x) > Mathf.Epsilon;
+        IsMovingVertical = Mathf.Abs(playerRigidbody.velocity.y) > Mathf.Epsilon;
 
-        if (moveInput.x > Mathf.Epsilon)
+        if ((value.isPressed && IsMovingHorizontal) || (value.isPressed && IsMovingVertical))
         {
-            transform.localScale = new Vector2(playerScaleX, transform.localScale.y);
+            playerAnimator.SetTrigger("isRolling");
+
+            moveSpeed += rollSpeedBoost;
+            StartCoroutine(ResetMoveSpeed(rollAnimTime));
+
+            playerRigidbody.AddForce(rollDistance * Mathf.Sign(transform.localScale.x), ForceMode2D.Impulse);
         }
-        else if (moveInput.x == 0)
+    }
+
+    IEnumerator ResetMoveSpeed(float value)
+    {
+        yield return new WaitForSeconds(value);
+
+        moveSpeed -= rollSpeedBoost;
+    }
+
+    void FlipSprite()
+    {
+        //if (IsMovingHorizontal)
+        //{
+        //    transform.localScale = new Vector2(Mathf.Sign(playerRigidbody.velocity.x), transform.localScale.y);
+        //    Debug.Log("velocity is:" + playerRigidbody.velocity);
+        //}
+
+        IsMovingHorizontal = Mathf.Abs(playerRigidbody.velocity.x) > Mathf.Epsilon;
+
+        if (IsMovingHorizontal)
         {
-            transform.localScale = new Vector2(transform.localScale.x, transform.localScale.y);
+            transform.localScale = new Vector2(Mathf.Sign(playerRigidbody.velocity.x), 1f);
         }
-        else
-        {
-            transform.localScale = new Vector2(-1 * playerScaleX, transform.localScale.y);
-        }
+
+
+        //if (moveInput.x != 0)
+        //{
+        //    if (moveInput.x > Mathf.Epsilon)
+        //    {
+        //        transform.localScale = new Vector2(1, 1);
+
+        //    }
+        //    else if (moveInput.x < -Mathf.Epsilon)
+        //    {
+        //        transform.localScale = new Vector2(-1, 1);
+        //    }
+        //}
 
     }
+
+    #endregion
 }
